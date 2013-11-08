5eab4c7b1cf8cd35594c81cce7fb18cb70b208f8
markj287 <markjohnson@Marks-MacBook-Pro.local>
2013-11-08 02:53:48 -0500

    added second view controller

---
+++ Project 2/week2_assignment/week2_assignment/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  week2_assignment
+//
+//  Created by Mark Johnson on 11/8/13.
+//  Copyright (c) 2013 Mark Johnson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
